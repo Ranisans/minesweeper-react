57ae991ef792ed6f3aeb4e0ee5cdcb987af77692
Rani <ranisans@yahoo.com>
2020-08-02 20:46:19 +0300

    feat: add sceleton to application add styles to app add app constants

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
-import './App.scss'
+import "./App.scss";
 
 const App: React.FC = () => {
-  return <div className="APP">
-    Hello from APP
-  </div>
-}
+  return (
+    <div className="APP">
+      <div className="header">Header</div>
+      <div className="game_field">Field</div>
+    </div>
+  );
+};
 
 export default App;
