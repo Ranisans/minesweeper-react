d0865c9c5fa33d47a399342e0f58f8c4492729b8
Ranisans <ranisans@yahoo.com>
2020-08-03 11:26:15 +0300

    feat: add base style to storybook add mixin to scss add Button

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+
+import Button from "./index";
+
+import { CellState, CellValue } from "../../types";
+
+export default {
+  title: "Button",
+  component: Button,
+};
+
+const callback = (row: number, col: number) => {
+  // eslint-disable-next-line no-console
+  console.log(row, col);
+};
+
+export const Base: React.FC = () => {
+  return (
+    <Button
+      col={0}
+      row={0}
+      onClick={callback}
+      onContext={callback}
+      state={CellState.open}
+      value={CellValue.eight}
+    />
+  );
+};
+
+export const Visible: React.FC = () => {
+  return (
+    <Button
+      col={0}
+      row={0}
+      onClick={callback}
+      onContext={callback}
+      state={CellState.visible}
+      value={CellValue.five}
+    />
+  );
+};
+
+export const Flagged: React.FC = () => {
+  return (
+    <Button
+      col={0}
+      row={0}
+      onClick={callback}
+      onContext={callback}
+      state={CellState.flagged}
+      value={CellValue.four}
+    />
+  );
+};
+
+export const Bomb: React.FC = () => {
+  return (
+    <Button
+      col={0}
+      row={0}
+      onClick={callback}
+      onContext={callback}
+      state={CellState.visible}
+      value={CellValue.bomb}
+    />
+  );
+};
+
+export const ExplodedBomb: React.FC = () => {
+  return (
+    <Button
+      col={0}
+      row={0}
+      onClick={callback}
+      onContext={callback}
+      state={CellState.visible}
+      value={CellValue.bomb}
+      red
+    />
+  );
+};
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+import { CellState, CellValue } from "../../types";
+
+import "./Button.scss";
+
+interface BProps {
+  value: CellValue;
+  state: CellState;
+  red?: boolean;
+  row: number;
+  col: number;
+  onClick: (row: number, col: number) => void;
+  onContext: (row: number, col: number) => void;
+}
+
+const Button: React.FC<BProps> = ({
+  value,
+  state,
+  red,
+  col,
+  row,
+  onClick,
+  onContext,
+}: BProps) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const renderedContent: any = () => {
+    if (state === CellState.visible) {
+      if (value === CellValue.bomb) {
+        return (
+          <span role="img" aria-label="bomb">
+            💣
+          </span>
+        );
+      }
+      if (value === CellValue.none) {
+        return null;
+      }
+      return value;
+    }
+    if (state === CellState.flagged) {
+      return (
+        <span role="img" aria-label="bomb">
+          🚩
+        </span>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <div
+      className={`button ${
+        state === CellState.visible ? "button--visible" : ""
+      } button-value-${value} ${red ? "button-red" : ""}
+    `}
+      role="button"
+      tabIndex={0}
+      aria-label="button"
+      onClick={() => {
+        onClick(row, col);
+      }}
+      onKeyPress={() => {}}
+      onContextMenu={() => {
+        onContext(row, col);
+      }}
+    >
+      {renderedContent()}
+    </div>
+  );
+};
+
+export default Button;
